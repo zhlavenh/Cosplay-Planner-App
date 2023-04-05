ed194f44f5d3a0c4dfe32b07f4515d5c4b921397
Zee Lavenhouse <Zeehlavenhouse@gmail.com>
2023-04-05 16:27:31 -0400

    pseudo coded crud function to be created

--- crud.py
+++ crud.py
@@ -13,6 +13,16 @@ def create_user(user_name, user_password, fname, lname, email):
     
     return user
 
+def create_new_outfit(outfit_name, public, notes, user_id, character_id):
+
+# Create a new outfit > req: select character, name | opt: add to new collectiomn, add to existing collection
+# Create new collection > req: select public status, name
+# Update existing outfit > change name, add/remove collection, delete outfit, add/remove items
+# Update collection > add/remove outfit, change name, change public status, delete collection
+
+
+
+
 if __name__ == "__main__":
     from server import app
 
